45d02e13a467e7b5ffdfbb33b2c86f3abef77b77
Prajwal H S <prajwalhs4india@gmail.com>
2025-04-29 12:27:04 +0530

    api changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Blog from "./components/Blog/Blog";
 import BlogContent from "./components/BlogContent/BlogContent";
+import HomePage from "./components/HomePage/HomePage";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./global.css";
-import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   const [blogs, setBlogs] = useState([]);
-  // const API_BASE_URL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
-
     const fetchBlogs = async () => {
       try {
         const res = await axios.get(
           `https://strapi-backend-f2jr.onrender.com/api/blogs?populate=*`,
           {
             headers: {
-              'Authorization': `Bearer  ${process.env.API_TOKEN}`, // Replace YOUR_API_KEY with the actual API key
+              Authorization: `Bearer ${process.env.API_TOKEN}`,
             },
           }
         );
@@ -27,7 +25,7 @@ function App() {
           id: item.id,
           link: item.attributes.BlogLink,
         }));
-        setBlogs(formattedBlogs); // Keep using setBlogs
+        setBlogs(formattedBlogs);
       } catch (err) {
         console.error("Error fetching blogs:", err);
       }
@@ -36,15 +34,17 @@ function App() {
     fetchBlogs();
   }, []);
 
-
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
+          {/* Static Routes First */}
           <Route path="/" element={<HomePage />} />
           <Route path="/blog" element={<Blog blogs={blogs} />} />
-          <Route path="*" element={<BlogContent />} />
+
+          {/* Dynamic Blog Route (Must be last to avoid conflict) */}
+          <Route path="/:blogLink" element={<BlogContent />} />
         </Routes>
       </BrowserRouter>
     </div>
