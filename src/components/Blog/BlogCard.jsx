0215eafdf8360083cb16166e50deee3c74b54db3
Prajwal H S <prajwalhs4india@gmail.com>
2025-04-29 13:14:54 +0530

    api token direct added

--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -15,10 +15,11 @@ const BlogCard = () => {
           `https://strapi-backend-f2jr.onrender.com/api/blogs?populate=*`,
           {
             headers: {
-              Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`, // Replace YOUR_API_KEY with the actual API key
+              Authorization: `Bearer cc9ae51c0a62b1e5f0088f2c313517fd0185226b3fb53738e9038a6c971154225adf50e8bb31e8c34cf027affb988a111edcfd596818d7e1498224676dbf0ef85dac0d07733f42f333a5c11a39486f963c9fdeee936f429cfc8592cdf052ef4f687f682d4129f2a5d4b43e182ddd620cdf8b4f23460948d396d6b012d91ba923`, // Replace YOUR_API_KEY with the actual API key
             },
           }
         );
+        console.log("Token used:", process.env.REACT_APP_API_TOKEN);
         console.log("Raw Blog Data:", res.data.data); // optional for debugging
 
         const formattedBlogs = res.data.data.map((item) => ({
